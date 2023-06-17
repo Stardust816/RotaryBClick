15f89891e247b267701b134bfb892c455210c630
stephanweidinger <ic21b064@technikum-wien.at>
2023-06-17 10:52:31 +0200

    Initial Commit

---
+++ Core/Src/main.c
@@ -0,0 +1,506 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+typedef StaticTask_t osStaticThreadDef_t;
+typedef StaticQueue_t osStaticMessageQDef_t;
+/* USER CODE BEGIN PTD */
+typedef struct  {
+  bool button_pressed;
+  int counter;
+} InputEvent;
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+#define LEDMIN 3
+#define LEDMAX 11
+#define LEDSTART 7
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+
+/* Definitions for LedTask */
+osThreadId_t LedTaskHandle;
+uint32_t LedTaskBuffer[ 128 ];
+osStaticThreadDef_t LedTaskControlBlock;
+const osThreadAttr_t LedTask_attributes = {
+  .name = "LedTask",
+  .cb_mem = &LedTaskControlBlock,
+  .cb_size = sizeof(LedTaskControlBlock),
+  .stack_mem = &LedTaskBuffer[0],
+  .stack_size = sizeof(LedTaskBuffer),
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for EncoderTask */
+osThreadId_t EncoderTaskHandle;
+uint32_t EncoderTaskBuffer[ 128 ];
+osStaticThreadDef_t EncoderTaskControlBlock;
+const osThreadAttr_t EncoderTask_attributes = {
+  .name = "EncoderTask",
+  .cb_mem = &EncoderTaskControlBlock,
+  .cb_size = sizeof(EncoderTaskControlBlock),
+  .stack_mem = &EncoderTaskBuffer[0],
+  .stack_size = sizeof(EncoderTaskBuffer),
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for RotaryEncoderQueue */
+osMessageQueueId_t RotaryEncoderQueueHandle;
+uint8_t RotaryEncoderQueueBuffer[ 16 * sizeof( InputEvent ) ];
+osStaticMessageQDef_t RotaryEncoderQueueControlBlock;
+const osMessageQueueAttr_t RotaryEncoderQueue_attributes = {
+  .name = "RotaryEncoderQueue",
+  .cb_mem = &RotaryEncoderQueueControlBlock,
+  .cb_size = sizeof(RotaryEncoderQueueControlBlock),
+  .mq_mem = &RotaryEncoderQueueBuffer,
+  .mq_size = sizeof(RotaryEncoderQueueBuffer)
+};
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+void StartLedTask(void *argument);
+void StartEncoderTask(void *argument);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void setleds(uint16_t ledring) {
+
+	//uint16_t ledring = 0b1010101010101010;
+
+	for (int i = 0; i < 16; i++) {
+
+		if (ledring & (1 << i)) {
+			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
+
+		} else {
+			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+		}
+
+		//SCK
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_SET);
+
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
+
+		//HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
+
+	}
+
+	//Latch
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Init scheduler */
+  osKernelInitialize();
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+	/* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+	/* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+	/* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* Create the queue(s) */
+  /* creation of RotaryEncoderQueue */
+  RotaryEncoderQueueHandle = osMessageQueueNew (16, sizeof(InputEvent), &RotaryEncoderQueue_attributes);
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+	/* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of LedTask */
+  LedTaskHandle = osThreadNew(StartLedTask, NULL, &LedTask_attributes);
+
+  /* creation of EncoderTask */
+  EncoderTaskHandle = osThreadNew(StartEncoderTask, NULL, &EncoderTask_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+	/* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+	/* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+  /* Start scheduler */
+  osKernelStart();
+  /* We should never get here as control is now taken by the scheduler */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	while (1) {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	}
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure LSE Drive Capability
+  */
+  HAL_PWR_EnableBkUpAccess();
+  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSE|RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = 0;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 16;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Enable MSI Auto calibration
+  */
+  HAL_RCCEx_EnableMSIPLLMode();
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|LD3_Pin|GPIO_PIN_5, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA0 PA3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB0 LD3_Pin PB5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|LD3_Pin|GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_StartLedTask */
+/**
+ * @brief  Function implementing the LedTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartLedTask */
+void StartLedTask(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+	//uint16_t value = 0;
+	InputEvent event;
+	/* Infinite loop */
+	for (;;) {
+		/*setleds(0b1010101010101010);
+		 osDelay(1000);
+		 setleds(0b0101010101010101);
+		 osDelay(1000);*/
+
+		osStatus_t ret = osMessageQueueGet(RotaryEncoderQueueHandle, &event, 0,
+				10);
+		if (ret != osOK) {
+			//something went wrong
+			printf("\r\ncannot read item from queue");
+		} else {
+			printf("\r\nread item");
+
+			uint32_t value;
+
+			if (event.button_pressed) { //If 6th bit set, button was pressed -> pause game -> display on leds
+				value = 0b1010101010101010;
+			} else {
+				value = 1 << event.counter;
+			}
+			setleds(value);
+		}
+
+		osDelay(2);
+	}
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_StartEncoderTask */
+/**
+ * @brief Function implementing the EncoderTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartEncoderTask */
+void StartEncoderTask(void *argument)
+{
+  /* USER CODE BEGIN StartEncoderTask */
+	uint16_t counter = 8;
+	//uint16_t old_counter = 0;
+	uint8_t old_state_a = 0;
+	uint8_t old_state_button = 0;
+	InputEvent event;
+	event.button_pressed = true;
+	event.counter = LEDSTART;
+	InputEvent oldevent;
+	/* Infinite loop */
+	for (;;) {
+
+		uint8_t a = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0);
+		uint8_t b = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1);
+		uint8_t button = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3);
+
+		if (a != old_state_a && event.counter <= 16) {
+
+			if (a != b) {
+				if (LEDMAX > event.counter)
+					event.counter++;
+
+			} else if (LEDMIN != event.counter) {
+				//if(counter != 3)
+				event.counter--;
+
+			}
+		}
+
+		if (old_state_button != button && button) {
+			event.button_pressed = !event.button_pressed; //Bit toggle -> XOR -> toggle 6th bit
+			event.counter = LEDSTART;
+			//setleds(0b1010101010101010);
+		}
+
+		if (memcmp(&oldevent, &event, sizeof(event))) {
+
+			char msg1[16];
+			snprintf(msg1, sizeof(msg1), "%d\r\n", event.counter);
+
+			HAL_UART_Transmit(&huart2, (uint8_t*) msg1, strlen(msg1), 1000);
+
+			//uint32_t leds = 0xFFFF0000;
+
+			//leds = leds >> counter;
+			//event.counter = counter;
+			osStatus_t ret = osMessageQueuePut(RotaryEncoderQueueHandle,
+					&event, 0, 10);
+
+			if (ret != osOK) {
+				//something went wrong
+				printf("\r\ncannot enqueue item %u", counter);
+			} else {
+				printf("\r\nenqueued item %u", counter);
+			}
+		}
+		old_state_a = a;
+		old_state_button = button;
+		//old_counter = counter;
+		memcpy(&oldevent, &event, sizeof(event));
+		osDelay(1);
+	}
+  /* USER CODE END StartEncoderTask */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
